656800c1ebe1504ea91a93ed12c9904340a11918
Mirwais Nezami <mirwais79@hotmail.com>
2020-10-07 13:30:30 +0200

    Icon component
    Bring in MaterialCommunityIcons and a component for icons

--- App.js
+++ App.js
@@ -4,16 +4,22 @@ import { StyleSheet, Text, SafeAreaView, Image, View } from 'react-native';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
+import ListItem from './app/component/ListItem';
 import AppText from './app/component/AppText';
 import AppButton from './app/component/AppButton';
 import Card from './app/component/Card';
 import ListDetailsScreen from './app/screens/ListingDetailsScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
+import Screen from './app/component/Screen';
+import Icon from './app/component/Icon';
 export default function App() {
 
 
   return (
-    <MessagesScreen />
+    <Screen>
+      <ListItem title='My title'
+        ImageComponent={<Icon name='email' />} />
+    </Screen>
   )
 }
 
