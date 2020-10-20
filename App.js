e4bfcd8ee9f535fb78b593254bb2abaf7bf3f31a
Mirwais Nezami <mirwais79@hotmail.com>
2020-10-20 10:57:15 +0200

    Add item edit screen
    Add a form for adding items, ListingEditScreen and a AppFormPicker

--- App.js
+++ App.js
@@ -1,29 +1,14 @@
 
 import React from 'react';
-import { StyleSheet, Text, SafeAreaView, Image, View, TextInput } from 'react-native';
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import WelcomeScreen from './app/screens/WelcomeScreen';
 
-import ListItem from './app/component/ListItem';
-import AppText from './app/component/AppText';
-import AppButton from './app/component/AppButton';
-import Card from './app/component/Card';
-import ListDetailsScreen from './app/screens/ListingDetailsScreen';
-import MessagesScreen from './app/screens/MessagesScreen';
-import Screen from './app/component/Screen';
-import Icon from './app/component/Icon';
-import AccountScreen from './app/screens/AccountScreen';
-import ListingsScreen from './app/screens/ListingsScreen';
-import AppTextInput from './app/component/AppTextInput';
-import AppPicker from './app/component/AppPicker';
-import LoginScreen from './app/screens/LoginScreen';
+import ListingEditScreen from './app/screens/ListingEditScreen';
 
 
 export default function App() {
 
 
   return (
-    <LoginScreen />
+    <ListingEditScreen />
   )
 }
 
---
+++ app/screens/ListingEditScreen.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import * as Yup from 'yup';
+
+import {
+  AppForm,
+  AppFormField,
+  AppFormPicker,
+  SubmitButton,
+} from '../component/forms';
+import Screen from '../component/Screen';
+
+const validationSchema = Yup.object().shape({
+  title: Yup.string().required().min(1).label('Title'),
+  price: Yup.number().required().min(1).max(10000).label('Price'),
+  description: Yup.string().label('Description'),
+  category: Yup.object().required().nullable().label('Category'),
+});
+
+const categories = [
+  { label: 'Furniture', value: 1 },
+  { label: 'Cars', value: 2 },
+  { label: 'Houses', value: 3 },
+];
+
+function ListingEditScreen() {
+  return (
+    <Screen style={styles.container}>
+      <AppForm
+        initialValues={{
+          title: '',
+          price: '',
+          description: '',
+          category: null,
+        }}
+        onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
+      >
+        <AppFormField maxLength={255} name='title' placeholder='Title' />
+        <AppFormField
+          keybordType='numeric'
+          maxLength={8}
+          name='price'
+          placeholder='Price'
+        />
+        <AppFormPicker
+          items={categories}
+          name='category'
+          placeholder='Category'
+        />
+        <AppFormField
+          maxLength={255}
+          multiline={true}
+          name='description'
+          numberOfLines={3}
+          placeholder='Description'
+        />
+        <SubmitButton title='Post' />
+      </AppForm>
+    </Screen>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10
+  }
+});
+
+export default ListingEditScreen;
