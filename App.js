754bbbe060741b497f794fe296bcde10d39c4aa8
Mirwais Nezami <mirwais79@hotmail.com>
2020-10-26 11:57:05 +0100

    ImageInput
    add an image input component

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
 
-import React from 'react';
+import React, { useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
+
 import ListItem from './app/component/ListItem';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 
 import ListingEditScreen from './app/screens/ListingEditScreen';
 import ListingsScreen from './app/screens/ListingsScreen';
-
+import Screen from './app/component/Screen';
+import ImageInput from './app/component/ImageInput';
+import { Button, Image } from 'react-native';
 
 export default function App() {
+  const [imageUri, setImageUri] = useState();
+
 
 
   return (
-    < ListingEditScreen />
+    <Screen>
+      <ImageInput imageUri={imageUri} onChangeImage={uri => setImageUri(uri)} />
+    </Screen>
+
   )
 }
 
